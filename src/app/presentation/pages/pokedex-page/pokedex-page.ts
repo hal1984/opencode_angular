3874bc07c877d42181e74e5bd0660ac9988543c5
Jorge Díaz Díaz <hal_1984@hotmail.com>
2026-05-24 19:16:37 +0200

    T018: Update PokedexPage with loading, error, and data states (closes #68)

--- src/app/presentation/pages/pokedex-page/pokedex-page.ts
+++ src/app/presentation/pages/pokedex-page/pokedex-page.ts
@@ -1,8 +1,13 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { PokedexStore } from '../../../application/stores/pokedex.store';
+import { PokemonCard } from '../../components/pokemon-card/pokemon-card';
 
 @Component({
   selector: 'app-pokedex-page',
   templateUrl: './pokedex-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [PokemonCard],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PokedexPage {}
+export class PokedexPage {
+  readonly store = inject(PokedexStore);
+}
