ccbeea8b23c36f1072239a605d2b60f7698f2711
Jorge Díaz Díaz <hal_1984@hotmail.com>
2026-05-24 19:12:32 +0200

    T006: Create PokedexPage shell component (closes #56)

---
+++ src/app/presentation/pages/pokedex-page/pokedex-page.ts
@@ -0,0 +1,8 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-page',
+  templateUrl: './pokedex-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokedexPage {}
