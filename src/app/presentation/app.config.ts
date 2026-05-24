207d86893d5a249c78fddd6fa01520bb58ce007a
Jorge Díaz Díaz <hal_1984@hotmail.com>
2026-05-24 19:09:58 +0200

    T002: Add providers to app.config.ts (closes #52)

--- src/app/presentation/app.config.ts
+++ src/app/presentation/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +9,9 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    { provide: IMAGE_LOADER, useValue: (config: ImageLoaderConfig) => config.src }
   ]
 };
