5a3c4bdbb400deaa6a8633c64f37a954da3a8adb
Jorge Díaz Díaz <hal_1984@hotmail.com>
2026-05-24 19:12:34 +0200

    T007: Add /pokedex lazy-loaded route (closes #57)

--- src/app/presentation/app.routes.ts
+++ src/app/presentation/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pokedex',
+    loadComponent: () => import('./pages/pokedex-page/pokedex-page').then(m => m.PokedexPage)
+  }
+];
