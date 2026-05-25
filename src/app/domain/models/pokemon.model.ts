7e9ab88b55cbd19d54cc0a94a19006a95abb3778
Jorge Díaz Díaz <hal_1984@hotmail.com>
2026-05-26 00:15:45 +0200

    T001-T002: Add PokemonDetail domain entities and getPokemonById to repository interface

--- src/app/domain/models/pokemon.model.ts
+++ src/app/domain/models/pokemon.model.ts
@@ -10,3 +10,28 @@ export interface PokemonListResponse {
   next: string | null;
   previous: string | null;
 }
+
+export interface PokemonType {
+  slot: number;
+  name: string;
+}
+
+export interface PokemonAbility {
+  name: string;
+}
+
+export interface PokemonStat {
+  name: string;
+  baseStat: number;
+}
+
+export interface PokemonDetail {
+  id: number;
+  name: string;
+  spriteUrl: string;
+  height: number;
+  weight: number;
+  types: PokemonType[];
+  abilities: PokemonAbility[];
+  stats: PokemonStat[];
+}
--- src/app/domain/repositories/pokemon.repository.ts
+++ src/app/domain/repositories/pokemon.repository.ts
@@ -1,8 +1,9 @@
 import { InjectionToken } from '@angular/core';
-import { PokemonListResponse } from '../models/pokemon.model';
+import { PokemonListResponse, PokemonDetail } from '../models/pokemon.model';
 
 export interface IPokemonRepository {
   getPokemonList(offset: number, limit: number): Promise<PokemonListResponse>;
+  getPokemonById(id: number): Promise<PokemonDetail>;
 }
 
 export const POKEMON_REPOSITORY = new InjectionToken<IPokemonRepository>('POKEMON_REPOSITORY');
